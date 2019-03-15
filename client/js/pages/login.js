d32ecfbd7510ef872884d42ebc20063ee7fc4787
fender13 <ej.fender@outlook.com>
2019-03-15 11:06:57 +0700

    LOGIN DAN REGISTER sudah jalan dari frontend ke server

---
+++ client/js/pages/login.js
@@ -0,0 +1,40 @@
+Vue.component('add-login', {
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    template: `
+    <div id="login-form" class="container">
+        <div class="row">
+            <form v-on:submit.prevent="$emit('submit-login', username, password)" class="container col s4 offset-s4" id="login-form-border">
+                <h1 class="center">Login</h1>
+                <div class="row">
+                    <div class="input-field inline col s12">
+                        <input v-model="username" id="email_inline" type="text" class="validate" />
+                        <label for="email_inline">Username</label>
+                        <span class="helper-text" data-error="wrong" data-success="right"></span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input v-model="password" id="password" type="password" class="validate" />
+                        <label for="password">Password</label>
+                    </div>
+                </div>
+                <div class="row center">
+                    <button class="btn waves-effect waves-hard black" type="submit" name="action">
+                        Login
+                    </button>
+                </div>
+                <div class="row center">
+                    <button class="btn waves-effect waves-hard black" type="submit" name="action">
+                        Register
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+    `
+})
