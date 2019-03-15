70a40114a40663a2d74ac570f8567b4e31f8e717
ekkacipta <ekkacipta@yahoo.com>
2019-03-15 09:10:51 +0700

    edit server router and edit multer and add new shareing controller

--- server/routers/sharingRouter.js
+++ server/routers/sharingRouter.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 const upload = require('../middlewares/upload')
+const shareController = require('../controllers/sharingController')
 
-router.post('/upload', upload.single('myFile'), (req, res) => {
-    console.log(req.file)
-    if (req.file) {
-        filename = req.file.filename;
-        path = req.file.path;
-    }
-})
+router.post('/upload', upload.single('myFile'), shareController.uploaded)
+
+router.get('/', shareController.findAllPhoto)
+router.put('/update/:id', shareController.updateName)
+router.delete('/delete/:id', shareController.delete)
+
+
 
 module.exports = router
